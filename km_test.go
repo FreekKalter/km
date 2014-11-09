6efbe64de36e0472471fe04636583c6252124e44
Freek Kalter <freek@kalteronline.org>
2014-11-09 16:15:55 +0100

    added some more config parsing tests

--- km_test.go
+++ km_test.go
@@ -6,4 +6,20 @@ func TestParseConfig(t *testing.T) {
 	if _, err := parseConfig("/afilethatprobablynotexists"); err == nil {
 		t.Error("expected error on invalid filename")
 	}
+	config, err := parseConfig("testdata/config.yml")
+	if err != nil {
+		t.Errorf("error parsing test data config: %s", err)
+	}
+	if config.Env != "testing" {
+		t.Errorf("envirnment expected to be 'testing', got:%s", config.Env)
+	}
+
+	config, err = parseConfig("testdata/invalid_yml.yml")
+	if config.Env != "" || config.Log != "" {
+		t.Error("invalid yaml should result in empty config struct")
+	}
+}
+
+func TestMain(t *testing.T) {
+	main()
 }
