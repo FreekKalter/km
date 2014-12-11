8e282c081f263774e4dbe1dcf7d4834e0b5a3870
Freek Kalter <freek@kalteronline.org>
2014-12-11 17:10:45 +0100

    god damnit

--- travis-install.sh
+++ travis-install.sh
@@ -6,11 +6,13 @@
 go get -t github.com/FreekKalter/km
 go get -t github.com/FreekKalter/km/lib
 
-go_version=$(go version)
-if [[ $(echo "$version" | grep -c "devel") -ge 0 ]]; then
-    go get -v golang.org/x/tools/cmd/cover
-elif [[ $(echo "$version" | grep -c "go1.4") -ge 0 ]]; then
-    go get -v golang.org/x/tools/cmd/cover
-else
-    go get -v code.google.com/p/go.tools/cmd/cover
-fi
+go get -v golang.org/x/tools/cmd/cover
+
+#go_version=$(go version)
+#if [[ $(echo "$version" | grep -c "devel") -ge 0 ]]; then
+    #go get -v golang.org/x/tools/cmd/cover
+#elif [[ $(echo "$version" | grep -c "go1.4") -ge 0 ]]; then
+    #go get -v golang.org/x/tools/cmd/cover
+#else
+    #go get -v code.google.com/p/go.tools/cmd/cover
+#fi
