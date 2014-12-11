782fe7ab3a6563fe2e47feab683031357ecaf6cd
Freek Kalter <freek@kalteronline.org>
2014-12-11 16:36:19 +0100

    test golang 1.4 with travis

--- travis-install.sh
+++ travis-install.sh
@@ -7,7 +7,10 @@ go get -t github.com/FreekKalter/km
 go get -t github.com/FreekKalter/km/lib
 
 go version | grep devel &>/dev/null
-if [ $? -eq 0 ]; then
+go_version=$(go version)
+if [[ $(echo "$version" | grep -qc "devel") -ge 0 ]]; then
+    go get -v golang.org/x/tools/cmd/cover
+elif [[ $(echo "$version" | grep -qc "go1.4") -ge 0 ]]; then
     go get -v golang.org/x/tools/cmd/cover
 else
     go get -v code.google.com/p/go.tools/cmd/cover
